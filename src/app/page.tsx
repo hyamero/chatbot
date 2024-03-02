2614fb7a15014f0778dd9165ad6604fcdfb82014
Dale B <daleban.dev@gmail.com>
2024-03-02 23:12:06 +0800

    feat: basic chatbot with streaming ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import Chat from "./chat";
+
+export const runtime = "edge";
+
+export default function Page() {
   return (
     <main className="flex h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <nav className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Chatbot Application built with&nbsp;
           <code className="font-mono font-bold">Next.js and OpenAI</code>
@@ -16,7 +20,8 @@ export default function Home() {
             By Hyamero
           </a>
         </div>
-      </div>
+      </nav>
+      <Chat />
     </main>
   );
 }
