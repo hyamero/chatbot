8872802ce196029f291eac3c20dfae72858f40b6
Dale B <daleban.dev@gmail.com>
2024-03-03 12:00:46 +0800

    feat: toggle screen theme modes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ToggleTheme } from "@/components/toggle-theme";
 import Chat from "./chat";
 
 export const runtime = "edge";
@@ -7,19 +8,20 @@ export default function Page() {
     <main className="flex h-screen flex-col items-center justify-between p-24">
       <nav className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Chatbot Application built with&nbsp;
-          <code className="font-mono font-bold">Next.js and OpenAI</code>
+          Chatbot Application by&nbsp;
+          <code className="font-mono font-bold">
+            <a
+              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+              href="https://github.com/hyamero"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @hyamero
+            </a>
+          </code>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://github.com/hyamero"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By Hyamero
-          </a>
-        </div>
+
+        <ToggleTheme />
       </nav>
       <Chat />
     </main>
