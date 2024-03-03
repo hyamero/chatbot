02ac3926d129cef5c2e17a9a0129261dca427c74
Dale B <daleban.dev@gmail.com>
2024-03-03 14:22:40 +0800

    feat: profile hover card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import { ToggleTheme } from "@/components/toggle-theme";
 import { Chat } from "@/components/chat";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 
 export const runtime = "edge";
 
@@ -14,19 +21,48 @@ export default function Page() {
 
 const Header = () => {
   return (
-    <header className="z-10 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    <header className="z-10 max-w-5xl font-mono text-sm">
+      <p className="flex justify-center border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:from-inherit rounded-xl border bg-gray-200 p-4 dark:bg-zinc-800/30">
         Chatbot Application by&nbsp;
-        <code className="font-mono font-bold">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://github.com/hyamero"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            @hyamero
-          </a>
-        </code>
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <code className="font-mono font-bold">
+              <a
+                className="flex place-items-center gap-2 pointer-events-auto p-0"
+                href="https://github.com/hyamero"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                @hyamero
+              </a>
+            </code>
+          </HoverCardTrigger>
+          <HoverCardContent sideOffset={25} className="w-80 mr-5">
+            <div className="flex justify-between space-x-4">
+              <Avatar>
+                <AvatarImage src="https://avatars.githubusercontent.com/u/112312511?s=200&v=4" />
+                <AvatarFallback>VC</AvatarFallback>
+              </Avatar>
+              <div className="space-y-3">
+                <div className="space-y-1">
+                  <h4 className="text-sm font-semibold">@hyamero</h4>
+                  <p className="text-sm">
+                    Full-Stack Developer Based in the Philippines
+                  </p>
+                </div>
+                <Button variant="outline">
+                  <a
+                    href="https://github.com/hyamero"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Follow on GitHub
+                  </a>
+                </Button>
+              </div>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
       </p>
     </header>
   );
